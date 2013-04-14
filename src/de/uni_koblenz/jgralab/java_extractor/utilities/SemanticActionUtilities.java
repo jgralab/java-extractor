f1e60a50a6ddb28877fbeaa65dcff3f6680a8766
Daniel Janke <danijank@uni-koblenz.de>
2013-04-14 19:09:53 +0200

    Finished type representation (reuse of already created TypeSpecifications required)

--- src/de/uni_koblenz/jgralab/java_extractor/utilities/SemanticActionUtilities.java
+++ src/de/uni_koblenz/jgralab/java_extractor/utilities/SemanticActionUtilities.java
@@ -1,8 +1,13 @@
 package de.uni_koblenz.jgralab.java_extractor.utilities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import de.uni_koblenz.edl.parser.Position;
 import de.uni_koblenz.jgralab.java_extractor.builder.Java5Builder;
 import de.uni_koblenz.jgralab.java_extractor.schema.program.Program;
+import de.uni_koblenz.jgralab.java_extractor.schema.type.specification.BuiltInType;
+import de.uni_koblenz.jgralab.java_extractor.schema.type.specification.BuiltInTypes;
 
 public class SemanticActionUtilities {
 
@@ -33,11 +38,39 @@ public class SemanticActionUtilities {
 	}
 
 	/*
+	 * BuiltInTypes
+	 */
+
+	private final Map<BuiltInTypes, BuiltInType> builtInTypeMap = new HashMap<BuiltInTypes, BuiltInType>();
+
+	public BuiltInType getBuiltInType(Position currentPosition,
+			Object builtInTypes) {
+		BuiltInTypes builtInTypes_ = (BuiltInTypes) builtInTypes;
+		BuiltInType result = builtInTypeMap.get(builtInTypes_);
+		if (result == null) {
+			result = (BuiltInType) graphBuilder.createVertex(BuiltInType.VC,
+					currentPosition);
+			result.set_type(builtInTypes_);
+			builtInTypeMap.put(builtInTypes_, result);
+		} else {
+			// update position to the current one because the AttributedEdges
+			// should note the correct positions
+			graphBuilder.getPositionsMap().put(result, currentPosition);
+		}
+		return result;
+	}
+
+	/*
 	 * further helping methods
 	 */
 
 	public String extractSimpleName(String qualifiedName) {
 		return qualifiedName.substring(qualifiedName.lastIndexOf(".") + 1);
+	}
+
+	public String resolveQualifiedName(Object simpleName) {
+		// TODO extend complete name resolution
+		return (String) simpleName;
 	}
 
 	// TODO link qualified names to types, packages, members (especially
